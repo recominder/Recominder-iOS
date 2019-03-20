7a7859a4ad7546082f09cda6211674d19188f386
erica2605@live.com <erica2605@live.com>
2019-03-20 15:45:45 -0700

    made apple watch extension

---
+++ Recaminder/RecominderWatch Extension/InterfaceController.swift
@@ -0,0 +1,33 @@
+//
+//  InterfaceController.swift
+//  RecominderWatch Extension
+//
+//  Created by ✨💖Erica Naglik💖✨ on 3/20/19.
+//  Copyright © 2019 Timofey Makhlay. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var runningLabel: WKInterfaceLabel!
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        runningLabel.setText("Running")
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
